8b6f40ce9bdc6f21ca1d53b4ec4b31043d3f920f
Clara Bessa <clarabessa@outlook.com>
2025-11-06 12:58:59 -0300

    feat(backend): adiciona model Project

---
+++ portfolio-backend/app/Models/Project.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    /**
+     * Os atributos que são atribuíveis em massa.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'cover_image_url',
+        'description',
+        'stack',
+        'links',
+    ];
+
+    /**
+     * Os atributos que devem ser convertidos para tipos nativos.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'stack' => 'array',
+        'links' => 'array',
+    ];
+}
